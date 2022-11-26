7958b12058e8280f116ae25b93d590af688f1365
pandamu1004 <91474654+pandamu1004@users.noreply.github.com>
2022-11-26 17:16:23 +0900

    Add files via upload

---
+++ src/components/Star.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import StarRate from './StarRate';
+
+class Star extends Component {
+  state = {
+    idx:0,
+    rating: 0,    
+    cacheIdx: 0,
+    cacheRating: 0,
+  }
+
+  _mouseOver = (e,i) => {
+    e.persist()
+    let offsetX = e.nativeEvent.offsetX; 
+    let clientX = e.target.clientWidth;
+
+    if(offsetX > clientX / 2){
+      let value = 2;
+      this.setState({
+        idx:i,
+        rating:value
+      });
+    }else{
+      let value = 1;
+      this.setState({
+        idx:i,
+        rating:value
+      });
+    }
+  }
+
+  handleChange = (i,v) => {
+    this.setState({
+      idx:0,
+      rating:0,
+      cacheIdx:i,
+      cacheRating:v
+    });
+  }
+
+  render() {
+    return (
+      <StarRate 
+        _mouseOver={this._mouseOver}
+        onChange={this.handleChange} 
+        idx={this.state.idx} 
+        rating={this.state.rating}         
+        cacheIdx={this.state.cacheIdx}         
+        cacheRating={this.state.cacheRating} 
+      />
+    );
+  }
+}
+
+export default Star;
