c9775dc1fd1120a180cb82eb822cdb2b6dc13e2b
hs-1771383-jeongwooyang <71862583+hs-1771383-jeongwooyang@users.noreply.github.com>
2022-11-26 22:12:15 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
 import GlobalStyle from "./styles/GlobalStyle";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <React.StrictMode>
-    <App />
     <GlobalStyle />
-    </React.StrictMode>
   </div>
 );
